b92256fdd2482d6adfde68f1fa709000a764ead9
samschool789@gmail.com <samschool789@gmail.com>
2022-06-05 21:29:59 -0500

    bcrypt functionality

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,7 @@
 const router = require('express').Router();
+
 const userRoutes = require('./user-routes.js');
 
-router.use('./users', userRoutes);
-
-
+router.use('/users', userRoutes);
 
 module.exports = router;
